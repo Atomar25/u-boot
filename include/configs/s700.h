c720ce94e501a240f7a0822d116283d0326d031d
Amit Singh Tomar <amittomer25@gmail.com>
2019-01-14 00:25:07 +0530

    arm: add support Actions Semi S700
    This patch adds basic support for Actions Semi based S700
    SoC, which is driven by common owl framework.
    
    Signed-off-by: Amit Singh Tomar <amittomer25@gmail.com>

---
+++ include/configs/s700.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ */
+
+#ifndef _CONFIG_S700_H_
+#define _CONFIG_S700_H_
+
+/*
+ * Include common owl configuration where most the settings are
+ */
+#include <configs/owl-common.h>
+
+#endif
