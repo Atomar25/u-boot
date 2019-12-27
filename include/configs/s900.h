59b4b343396fa3bf3e5710479ffa4ba54b69821c
Amit Singh Tomar <amittomer25@gmail.com>
2019-12-28 02:02:47 +0530

    arm: actions: Add common framework for Actions Owl Semi SoCs
    This commit adds common arch support for Actions Semi Owl
    series SoCs and removes the Bubblegum96 board files.
    
    Signed-off-by: Amit Singh Tomar <amittomer25@gmail.com>

---
+++ include/configs/s900.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2015 Actions Semi Co., Ltd.
+ * Copyright (C) 2018 Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
+ *
+ */
+
+#ifndef _CONFIG_S900_H_
+#define _CONFIG_S900_H_
+
+/*
+ * Include common owl configuration where most the settings are
+ */
+#include <configs/owl-common.h>
+
+#endif
